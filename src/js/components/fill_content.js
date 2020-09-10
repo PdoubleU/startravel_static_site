21d313c15f4ed4141adde6fd9a70c5801ceefb0e
PdoubleU <piotrwitasik1988@gmail.com>
2020-09-10 21:47:13 +0200

    1. add method to fill_content.js which is dynamically creating the content of subpages

--- src/js/components/fill_content.js
+++ src/js/components/fill_content.js
@@ -8,21 +8,27 @@ class FillContent {
         this.selector = classNameSelector;
         this.file = this.options.file;
         this.country = this.options.country;
-        this.randomNo = null
+        this.randomNo = 5;
 
-        this.generateHTMLTags();
+        this.generateHTMLTags(this.randomNo);
     }
-    generateHTMLTags() {
+    generateHTMLTags(counter) {
         if(document.getElementById(this.selector) == undefined) {
             return 0;
         }
-        var element = document.getElementById(this.selector);
-        element.classList.add('red_box');
+        for (let i=0; i < counter; i++){
+            const CONTENT_ELEM = document.createElement("div");
+            CONTENT_ELEM.classList.add('element');
+            var Content_text = document.createTextNode('wycieczka 1');
+            CONTENT_ELEM.appendChild(Content_text);
+            var element = document.getElementById(this.selector);
+            element.appendChild(CONTENT_ELEM);
+        }
     }
 }
 
-new FillContent('germany');
-new FillContent('poland');
-new FillContent('czechia');
+new FillContent('content-de');
+new FillContent('content-pl');
+new FillContent('content-cz');
 
 export default {FillContent};

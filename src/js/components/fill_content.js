254ae9355ea389fe832f8c368f1aaa1669f9f126
PdoubleU <piotrwitasik1988@gmail.com>
2020-09-11 17:31:56 +0200

    1. change the execution order in fill_content: first loadData(), second generateHTLMTags(); 2. add options to each call of FillContent; 3. develope loadData() to handle with XMLHttprequests - ask for data from server, if success then execute generateHTMLTags() to create div elements with proper content;

--- src/js/components/fill_content.js
+++ src/js/components/fill_content.js
@@ -1,34 +1,58 @@
 class FillContent {
     constructor(classNameSelector, opts) {
         const DEFAULT_OPTS = {
-            file: "offer-en.json",
             country : "pl",
+            _path: '../json/offer-pl.json'
         };
         this.options = Object.assign({}, DEFAULT_OPTS, opts);
         this.selector = classNameSelector;
-        this.file = this.options.file;
         this.country = this.options.country;
-        this.randomNo = 5;
+        this.path = this.options._path;
+        this.data = null;
 
-        this.generateHTMLTags(this.randomNo);
+        this.loadData(this.generateHTMLTags);
     }
-    generateHTMLTags(counter) {
-        if(document.getElementById(this.selector) == undefined) {
-            return 0;
-        }
-        for (let i=0; i < counter; i++){
+    generateHTMLTags(counter, objJSON, selector, country) {
+        for (let i = 0; i < counter; i++){
+            var newContent = objJSON[country][i].title;
             const CONTENT_ELEM = document.createElement("div");
             CONTENT_ELEM.classList.add('element');
-            var Content_text = document.createTextNode('wycieczka 1');
+            var Content_text = document.createTextNode(newContent);
             CONTENT_ELEM.appendChild(Content_text);
-            var element = document.getElementById(this.selector);
+            var element = document.getElementById(selector);
             element.appendChild(CONTENT_ELEM);
         }
     }
+    loadData(callback){
+        if(document.getElementById(this.selector) == undefined) {
+            return 0;
+        }
+        var xhr = new XMLHttpRequest();
+        var _path = this.path;
+        var selector = this.selector;
+        var country = this.country;
+        xhr.open('GET', _path, true);
+        xhr.onload = function() {
+            if(xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                return callback(5, response, selector, country);
+            }
+        }
+        xhr.send(null);
+    }
 }
 
-new FillContent('content-de');
-new FillContent('content-pl');
-new FillContent('content-cz');
+new FillContent('content-de', {
+                country : "de",
+                _path: '../json/offer-pl.json'
+            });
+new FillContent('content-pl',{
+                country : "pl",
+                _path: '../json/offer-pl.json'
+            });
+new FillContent('content-cz',{
+                country : "cz",
+                _path: '../json/offer-pl.json'
+            });
 
 export default {FillContent};

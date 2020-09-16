14f69c2acaaacb37b9dd511e5c3765af9a99ef83
PdoubleU <piotrwitasik1988@gmail.com>
2020-09-16 13:47:25 +0200

    1. create scrl_up_btn.js - file which deals with the scrolling on the page and create additional button to scroll up the page in case of mobile devices 2. style a scrl_up button

---
+++ src/js/components/scrl_up_btn.js
@@ -0,0 +1,30 @@
+let width = window.innerWidth;
+let grip = document.getElementsByClassName('main');
+
+export default function scroll() {
+
+    if (width <= 780){
+        var SCRL_BTN = document.createElement('button');
+        SCRL_BTN.className = 'scroll_btn';
+        SCRL_BTN.setAttribute('aria-label', 'scroll up button');
+        grip[0].appendChild(SCRL_BTN);
+
+        let scroll_btn = document.getElementsByClassName('scroll_btn');
+
+        if (window.scrollY > 500) {
+            scroll_btn[0].classList.add('scroll_btn--active');
+        }
+        else {
+            scroll_btn[0].classList.remove('scroll_btn--active');
+        }
+
+        scroll_btn[0].onclick = function scrollTop() {
+                                    window.scrollTo({
+                                        top: 0,
+                                        left: 100,
+                                        behavior: 'smooth'
+                                        });
+        }
+    }
+}
+window.onscroll = scroll;
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
 import {FillContent} from "./components/fill_content.js";
 import {Slider} from "./components/slider.js";
+import { scroll } from "./components/scrl_up_btn.js";

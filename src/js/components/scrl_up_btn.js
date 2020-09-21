e64829e60d87a4c64f8d40a56bd3e20afe5b8b45
PdoubleU <piotrwitasik1988@gmail.com>
2020-09-21 22:34:40 +0200

    1. remove bug from scroll_up_bt function - refactoring mostly

--- src/js/components/scrl_up_btn.js
+++ src/js/components/scrl_up_btn.js
@@ -1,29 +1,35 @@
 let grip = document.getElementsByClassName('main');
+var isButton = false;
 
-export default function scroll() {
-    let width = window.innerWidth;
-    if (width <= 780){
-        var SCRL_BTN = document.createElement('button');
-        SCRL_BTN.className = 'scroll_btn';
-        SCRL_BTN.setAttribute('aria-label', 'scroll up button');
-        grip[0].appendChild(SCRL_BTN);
-
-        let scroll_btn = document.getElementsByClassName('scroll_btn');
-
+export function scroll() {
+        var scroll_btn = document.getElementsByClassName('scroll_btn');
+        if (scroll_btn == undefined) {return 0;}
         if (window.scrollY > 500) {
             scroll_btn[0].classList.add('scroll_btn--active');
         }
         else {
             scroll_btn[0].classList.remove('scroll_btn--active');
         }
-
         scroll_btn[0].onclick = function scrollTop() {
                                     window.scrollTo({
                                         top: 0,
                                         left: 100,
                                         behavior: 'smooth'
                                         });
-        }
+                                }
+
+}
+export function createButton() {
+    if (isButton === true) {return 0;}
+    var width = window.innerWidth;
+    if (width <= 780 ) {
+        isButton =  true;
+        var SCRL_BTN = document.createElement('button');
+        SCRL_BTN.className = 'scroll_btn';
+        SCRL_BTN.setAttribute('aria-label', 'scroll up button');
+        grip[0].appendChild(SCRL_BTN);
     }
 }
+window.onresize = createButton;
+window.onload = createButton;
 window.onscroll = scroll;
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 import {FillContent} from "./components/fill_content.js";
 import {Slider} from "./components/slider.js";
-import { scroll } from "./components/scrl_up_btn.js";
+import { scroll, createButton } from "./components/scrl_up_btn.js";

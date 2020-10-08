e82ea28923f448262f2bfb9f07ba8d77eba33667
PdoubleU <piotrwitasik1988@gmail.com>
2020-10-08 18:46:40 +0200

    1. create class folder for reusable modules 2. move the ModalWindow class from open_modal_form.js to new file ModalWindow.js in classes folder 3. add parameter 'options' for the ModalWindow.js to handle with diffirent kinds of modal windows on the page

--- src/js/components/open_modal_form.js
+++ src/js/components/open_modal_form.js
@@ -1,55 +1,7 @@
-class ModalWindow {
-    constructor(id){
-        this.id = id;
-
-        this.generateHTMLTags();
-    }
-    generateHTMLTags() {
-        let grip = document.getElementsByTagName('body');
-
-        const MODAL_ARTICLE = document.createElement('article');
-        MODAL_ARTICLE.id = 'modal_' + this.id;
-        grip[0].appendChild(MODAL_ARTICLE);
-
-        const MODAL_CONTAINER = document.createElement('div');
-        MODAL_CONTAINER.classList.add('modal');
-        MODAL_CONTAINER.classList.add('form');
-        grip[0].lastChild.appendChild(MODAL_CONTAINER);
-
-        const MODAL_BACKDROP = document.createElement('div');
-        MODAL_BACKDROP.classList.add('modal_backdrop');
-        MODAL_BACKDROP.classList.add('form');
-        grip[0].lastChild.children[0].appendChild(MODAL_BACKDROP);
-
-        const MODAL_BODY = document.createElement('div');
-        MODAL_BODY.classList.add('modal_body');
-        MODAL_BODY.classList.add('form');
-        grip[0].lastChild.children[0].appendChild(MODAL_BODY);
-
-        const ACTION_BTN = document.createElement('button');
-        ACTION_BTN.classList.add('modal_btn');
-        ACTION_BTN.classList.add('form');
-        ACTION_BTN.appendChild(document.createTextNode('Wyślij'));
-        grip[0].lastChild.children[0].lastChild.appendChild(ACTION_BTN);
-
-        const MODAL_CONTENT = document.createElement('div');
-        MODAL_CONTENT.classList.add('content-container');
-        MODAL_CONTENT.appendChild(document.createTextNode('here will be form'));
-        grip[0].lastChild.children[0].lastChild.appendChild(MODAL_CONTENT);
-
-        const CLOSE_BTN = document.createElement('button');
-        CLOSE_BTN.classList.add('modal_close');
-        CLOSE_BTN.classList.add('form');
-        grip[0].lastChild.children[0].lastChild.appendChild(CLOSE_BTN);
-        CLOSE_BTN.onclick = () => {MODAL_ARTICLE.remove()};
-    }
-}
+import { ModalWindow } from "../classes/ModalWindow.js";
 
 const openTrigger = document.getElementsByClassName('open-form');
 
-openTrigger[0].onclick = () => {
+export default openTrigger[0].onclick = () => {
     new ModalWindow('form');
 };
-
-
-export default ModalWindow;
--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,5 @@ import { FillContent } from "./components/fill_content.js";
 import { Slider } from "./components/slider.js";
 import { scroll, createButton } from "./components/scrl_up_btn.js";
 import { close_mobile } from "./components/close_mobile_nav";
-import { ModalWindow } from "./components/open_modal_form.js";
+import { openTrigger } from "./components/open_modal_form.js";
 import { showGDPR } from "./components/bar_gdpr.js";

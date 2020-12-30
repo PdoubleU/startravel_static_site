84851b773c86a12f968ce266ff8c15313c26feee
PdoubleU <piotrwitasik1988@gmail.com>
2020-12-30 21:58:12 +0100

    1. add tag style in index,html to deal with two things: 1. when user is trying to open site on IE he get message that the IE browser is not supported; 2. set html style parameter display to none - this prevents FOUC event 2. other documents untouched

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 module.exports = {
-  plugins:
-    [
-        require('autoprefixer')({
-            'browsers': ['> 1%', 'last 2 versions']
-        }),
-    require('cssnano')
-    ]
-}
+	plugins: [
+		require('autoprefixer')({
+			browsers: [ '> 1%', 'last 2 versions' ]
+		}),
+		require('cssnano')
+	]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -7,90 +7,85 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: [
-    'babel-polyfill',
-    'fetch-ponyfill',
-    './src/js/index.js',
-    './src/style/index.scss',
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
-  target: 'web',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { url: false}
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              config: {
-                path: 'postcss.config.js'
-              }
-            }
-          },
-          {
-            loader: 'sass-loader'
-          },
-
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: ['svg-inline-loader',
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name].[ext]',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.bundle.css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/subpages/poland.html',
-      filename: './subpages/poland.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/subpages/czechia.html',
-      filename: './subpages/czechia.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/subpages/germany.html',
-      filename: './subpages/germany.html'
-    }),
-  ],
+	entry: [ 'babel-polyfill', 'fetch-ponyfill', './src/js/index.js', './src/style/index.scss' ],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.bundle.js'
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000
+	},
+	target: 'web',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { url: false }
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							config: {
+								path: 'postcss.config.js'
+							}
+						}
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				use: [
+					'svg-inline-loader',
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: 'images',
+							name: '[name].[ext]'
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.bundle.css'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/subpages/poland.html',
+			filename: './subpages/poland.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/subpages/czechia.html',
+			filename: './subpages/czechia.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/subpages/germany.html',
+			filename: './subpages/germany.html'
+		})
+	]
 };

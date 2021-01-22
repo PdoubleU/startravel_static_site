bc2b4f1d6dae5eaa1c833b0cae21c89f42f5df37
PdoubleU <piotrwitasik1988@gmail.com>
2021-01-22 13:03:45 +0100

    1. refactor reveal_txt js file, just change getElement... on querySelector

--- src/js/components/reveal_txt.js
+++ src/js/components/reveal_txt.js
@@ -1,10 +1,10 @@
-const elem = document.getElementsByClassName('text');
+let elem = document.querySelectorAll('.text');
 
 export const REVEAL_TXT = () => {
-    for (let i = 0; i < elem.length; i++) {
-        let timeout = Math.floor(Math.random() * 10);
-        elem[i].children[0].classList.add('reveal', '_' + timeout);
-    }
-}
+	for (let i = 0; i < elem.length; i++) {
+		let timeout = Math.floor(Math.random() * 10);
+		elem[i].children[0].classList.add('reveal', '_' + timeout);
+	}
+};
 
 window.onload = REVEAL_TXT;

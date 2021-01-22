a64ad311cf36908c39b516d0cbae9b9ac534b29f
PdoubleU <piotrwitasik1988@gmail.com>
2021-01-22 12:39:03 +0100

    1. change names of js files and set them to snake_case 2. refactor code in scrl_up_btn: in place of anonymous functions set arrow func., change all names var names from snake_case to camelCase; change getElementsByTag/Class name into querySelectorxy

--- src/js/components/scrl_up_btn.js
+++ src/js/components/scrl_up_btn.js
@@ -1,47 +1,47 @@
-const GRIP = document.getElementsByClassName('main');
-let isButton = false;
+const GRIP = document.querySelector('main');
+let isButtonActive = false;
 
 export const SCROLL = () => {
-        let scroll_btn = document.getElementsByClassName('scroll_btn');
-        if (isButton == false) {return 0;}
-        if (window.scrollY > 500) {
-            scroll_btn[0].classList.add('scroll_btn--active');
-        }
-        else {
-            scroll_btn[0].classList.remove('scroll_btn--active');
-        }
-        scroll_btn[0].onclick = function scrollTop() {
-                                    window.scrollTo({
-                                        top: 0,
-                                        left: 100,
-                                        behavior: 'smooth'
-                                        });
-                                }
-
-}
+	let scrollBtn = document.querySelector('.scroll_btn');
+	if (isButtonActive == false) {
+		return 0;
+	}
+	if (window.scrollY > 500) {
+		scrollBtn.classList.add('scroll_btn--active');
+	} else {
+		scrollBtn.classList.remove('scroll_btn--active');
+	}
+	scrollBtn.onclick = () => {
+		window.scrollTo({
+			top: 0,
+			left: 100,
+			behavior: 'smooth'
+		});
+	};
+};
 export const CREATE_BUTTON = () => {
-    if (isButton === true) {return 0;}
-    let width = window.innerWidth;
-    if (width <= 780 ) {
-        isButton =  true;
-        let SCRL_BTN = document.createElement('button');
-        let ARROW_TOP = document.createElement('div');
-        let ARROW_BOTTOM = document.createElement('div');
-        let ARROW_CONTAINER = document.createElement('div');
-        ARROW_TOP.className = 'arrow-class--top';
-        ARROW_BOTTOM.className = 'arrow-class--bottom';
-        ARROW_CONTAINER.className = 'arrow-class';
-        SCRL_BTN.className = 'scroll_btn';
-        SCRL_BTN.setAttribute('aria-label', 'Przewiń do góry');
-        SCRL_BTN.setAttribute('tabindex', '0');
-        SCRL_BTN.setAttribute('title', 'Przewiń do góry');
-        ARROW_CONTAINER.appendChild(ARROW_TOP);
-        ARROW_CONTAINER.appendChild(ARROW_BOTTOM);
-        SCRL_BTN.appendChild(ARROW_CONTAINER);
-        GRIP[0].appendChild(SCRL_BTN);
-    }
-}
+	if (isButtonActive === true) {
+		return 0;
+	}
+	if (window.innerWidth <= 780) {
+		isButtonActive = true;
+		let scrlBtn = document.createElement('button');
+		let arrowTop = document.createElement('div');
+		let arrowBot = document.createElement('div');
+		let arrowCont = document.createElement('div');
+		arrowTop.className = 'arrow-class--top';
+		arrowBot.className = 'arrow-class--bottom';
+		arrowCont.className = 'arrow-class';
+		scrlBtn.className = 'scroll_btn';
+		scrlBtn.setAttribute('aria-label', 'Przewiń do góry');
+		scrlBtn.setAttribute('tabindex', '0');
+		scrlBtn.setAttribute('title', 'Przewiń do góry');
+		arrowCont.appendChild(arrowTop);
+		arrowCont.appendChild(arrowBot);
+		scrlBtn.appendChild(arrowCont);
+		GRIP.appendChild(scrlBtn);
+	}
+};
 window.onresize = CREATE_BUTTON;
 window.onload = CREATE_BUTTON;
 window.onscroll = SCROLL;
-

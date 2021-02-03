40231327ded2ea585ef82aec2718a21b9fbfc49e
PdoubleU <piotrwitasik1988@gmail.com>
2021-02-03 23:05:06 +0100

    1. add assets, update src paths for background images etc.

--- src/js/components/open_modal_form.js
+++ src/js/components/open_modal_form.js
@@ -1,10 +1,12 @@
 import { ModalWindow } from '../classes/ModalWindow.js';
 
-const OPEN_MODAL = document.querySelector('#open-form');
+const OPEN_MODAL = document.querySelectorAll('#open-form');
 
-export default OPEN_MODAL.addEventListener('click', () => {
-	new ModalWindow('form', {
-		isActionBtn: false,
-		_path: '/json/form_content.json'
-	});
-});
+export default OPEN_MODAL.forEach((e) =>
+	e.addEventListener('click', () => {
+		new ModalWindow('form', {
+			isActionBtn: false,
+			_path: '/json/form_content.json'
+		});
+	})
+);
